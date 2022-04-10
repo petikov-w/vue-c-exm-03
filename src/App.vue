0455f8c5ec50b89894d49e5ed0cac6fad4e5a54b
Petikov Vladimir <developer-krox@yandex.ru>
2022-04-10 18:29:39 +0300

    commit-100422-1830 - Созданы компонент PostForm и PostList.

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,43 @@
 <template>
   <div>
 <!--    <Like></Like>-->
-    <Post></Post>
+    <PostForm></PostForm>
+    <PostList></PostList>
   </div>
 </template>
 
 <script>
 
 // import Like from "./components/v-Like"
-import Post from "./components/v-Post"
+import PostForm from "./components/PostForm"
+import PostList from "./components/PostList"
 export default {
   name: 'App',
   components: {
     // Like,
-    Post
+    PostList,
+    PostForm
+  },
+  data() {
+    return {
+      posts: [
+        {id: 1, title: 'JavaScript', body: 'Описание поста'},
+        {id: 2, title: 'JavaScript 1', body: 'Описание поста'},
+        {id: 3, title: 'JavaScript 2', body: 'Описание поста'},
+      ]
+    }
+  },
+  methods: {
+    createPost() {
+      const newPost = {
+        id: Date.now(),
+        title: this.title,
+        body: this.body
+      }
+      this.posts.push(newPost);
+      this.title = "";
+      this.body = "";
+    },
   }
 }
 </script>
---
+++ src/components/PostForm.vue
@@ -0,0 +1,53 @@
+<template>
+  <form @submit.prevent>
+    <h2>Создание поста</h2>
+    <input :value="title" @input="title=$event.target.value" type="text" placeholder="Название">
+    <input :value="body" @input="body=$event.target.value" type="text" placeholder="Описание">
+    <button class="btn" @click="createPost">Создать</button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "PostForm",
+  // methods: {
+  //   createPost() {
+  //     const newPost = {
+  //       id: Date.now(),
+  //       title: this.title,
+  //       body: this.body
+  //     }
+  //     this.posts.push(newPost);
+  //     this.title = "";
+  //     this.body = "";
+  //   }
+  // },
+}
+</script>
+
+<style lang="scss" scoped>
+  form {
+    display: flex;
+    flex-direction: column;
+    h2 {
+      margin: 15px 0;
+      color: blue;
+    }
+    input {
+      border: 2px solid green;
+      margin-bottom: 20px;
+      padding: 10px;
+    }
+  }
+  .btn {
+    margin-bottom: 20px;
+    padding: 10px 15px;
+    align-self: flex-end;
+    background-color: #d0cccc;
+    border: 0;
+    &:hover {
+      background-color: grey;
+      color: white;
+    }
+  }
+</style>
