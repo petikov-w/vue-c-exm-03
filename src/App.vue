e4153ffec91358e57871f6a17666bda3e0339850
krox2 <developer-krox@yandex.ru>
2022-04-10 12:23:13 +0300

    commit-100422-1230 - Продолжаю работу с  vue-c-exm-03 в ветке dev-1

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,33 @@
 <template>
   <div>
-    <Like></Like>
+<!--    <Like></Like>-->
+    <Post></Post>
   </div>
 </template>
 
 <script>
 
-import Like from "./components/v-Like"
+// import Like from "./components/v-Like"
+import Post from "./components/v-Post"
 export default {
   name: 'App',
   components: {
-    Like
+    // Like,
+    Post
   }
 }
 </script>
 
 <style>
 #app {
+  padding: 20px;
 }
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: sans-serif;
+  font-size: 18px;
+}
+
 </style>
